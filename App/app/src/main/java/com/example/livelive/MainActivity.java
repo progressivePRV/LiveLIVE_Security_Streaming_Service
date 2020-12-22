85672ae6c57c24d0ae3bce649663bc343d9e7327
Prabhav Jani <31888813+progressivePRV@users.noreply.github.com>
2020-12-22 16:22:26 -0500

    added countdowntime.cancel in mainActivity

--- App/app/src/main/java/com/example/livelive/MainActivity.java
+++ App/app/src/main/java/com/example/livelive/MainActivity.java
@@ -148,6 +148,7 @@ public class MainActivity extends AppCompatActivity implements Helper.InteractWi
     @Override
     protected void onStop() {
         super.onStop();
+        cdt.cancel();
         if (helper==null){
             return;
         }
